5b09da20c6d32014f7df37f1d6b4be00cc5df3a7
Brian Egan <brianjoseph.egan@gmail.com>
2020-08-30 22:43:11 -0700

    Moving Paddle
    Added Paddle prefab and currently moving unconstrained along only the Y axis.

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+
+    Rigidbody2D paddleRigidbody2D;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        paddleRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical"); // value between -1 and +1
+        Vector2 paddleVelocity = new Vector2(paddleRigidbody2D.velocity.x, controlThrow * moveSpeed);
+        paddleRigidbody2D.velocity = paddleVelocity;
+    }
+}
