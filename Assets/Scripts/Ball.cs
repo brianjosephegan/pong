df6035fdb73b34335cfac501e9935112465bb4bd
Brian Egan <brianjoseph.egan@gmail.com>
2020-08-31 19:07:48 -0700

    Sound Effect on Ball Collision
    Add an AudioSource to Ball Prefab and play a SFX whenever the Ball hits something.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,12 +7,15 @@ public class Ball : MonoBehaviour
     [SerializeField] float xPush = -10.0f;
     [SerializeField] float yPush = 0f;
     [SerializeField] float randomFactor = 0.2f;
+    [SerializeField] AudioClip ballCollisionSFX;
 
+    AudioSource ballAudioSource;
     Rigidbody2D ballRigidbody2D;
 
     // Start is called before the first frame update
     private void Start()
     {
+        ballAudioSource = GetComponent<AudioSource>();
         ballRigidbody2D = GetComponent<Rigidbody2D>();
         ballRigidbody2D.velocity = new Vector2(xPush, yPush);
     }
@@ -25,6 +28,8 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        ballAudioSource.PlayOneShot(ballCollisionSFX);
+
         Vector2 velocityTweak = new Vector2(Random.Range(0, randomFactor), Random.Range(0, randomFactor));
         ballRigidbody2D.velocity += velocityTweak;
     }
