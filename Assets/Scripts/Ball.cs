ed5b4c0cad0f4a88298793a79fb35b7061ec9a64
Brian Egan <brianjoseph.egan@gmail.com>
2020-08-31 17:05:34 -0700

    Introduce Moving Ball
    Prefab a Ball that can be launched at the Paddle. Collision still to be implemented.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] float xPush = -10.0f;
+    [SerializeField] float yPush = 0f;
+
+    Rigidbody2D ballRigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRigidbody2D = GetComponent<Rigidbody2D>();
+        ballRigidbody2D.velocity = new Vector2(xPush, yPush);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
