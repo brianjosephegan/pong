1308d2c7a3940aef3179ff0e3ef4c9aed24ac38f
Brian Egan <brianjoseph.egan@gmail.com>
2020-08-31 17:38:06 -0700

    Collisions
    Create the Play Space prefab - four walls that detect and react to collisions from the Ball.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,6 +6,7 @@ public class Ball : MonoBehaviour
 {
     [SerializeField] float xPush = -10.0f;
     [SerializeField] float yPush = 0f;
+    [SerializeField] float randomFactor = 0.2f;
 
     Rigidbody2D ballRigidbody2D;
 
@@ -20,5 +21,11 @@ public class Ball : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 velocityTweak = new Vector2(Random.Range(0, randomFactor), Random.Range(0, randomFactor));
+        ballRigidbody2D.velocity += velocityTweak;
     }
 }
